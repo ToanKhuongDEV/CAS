b6bcbb0b8410096f573cb651a45a962a5562c6ce
KhuongXuanToan（トアン） <toan.khuong.xuan@plan-b.co.jp>
2026-09-03 11:20:41 +0700

    update(fe): allow Cloudinary menu images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+        pathname: "/dh6qzqf73/image/upload/**",
+        protocol: "https",
+      },
+    ],
+  },
   reactStrictMode: true,
   output: "standalone",
 };
